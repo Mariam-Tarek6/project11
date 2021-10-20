17a1fcee902d333240384c70ae5512121167a33f
user <mtarek5999@gmaij.com>
2021-10-20 15:34:59 +0200

    first commit

---
+++ Project11/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>;
+using namespace std;
+void main() {
+	int x, y, sum;
+	cout << "enter x and y"<<'\n';
+	cin >> x;
+	cin >> y;
+	sum = x + y;
+	cout << sum;
+
+	system("pause");
+}
